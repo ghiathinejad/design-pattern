22317fdd2ce8abd807318fcdf15d64cc402e036b
ghiathinejad <m.ghiathinejad@gmail.com>
2022-12-10 13:52:53 +0330

    7-singleton

---
+++ singleton/ConnectDb.php
@@ -0,0 +1,26 @@
+<?php
+class ConnectDb {
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = 'root';
+    private $dbname = 'sokanacademy';
+
+    private function __construct() {
+        $this->conn = new PDO("mysql:host={$this->host};dbname = {$this-> dbname}",
+            $this->user, $this->pass,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new ConnectDb();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
---
+++ singleton/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'ConnectDb.php';
+
+$instance = ConnectDb::getInstance();
+$conn = $instance->getConnection();
+var_dump($conn);
+
+$instance = ConnectDb::getInstance();
+$conn = $instance->getConnection();
+var_dump($conn);
+
+$instance = ConnectDb::getInstance();
+$conn = $instance->getConnection();
+var_dump($conn);
